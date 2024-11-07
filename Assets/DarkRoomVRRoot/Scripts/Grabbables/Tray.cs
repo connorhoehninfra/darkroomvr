f9d631ad4bb7dcfca5243d7c5d5495591b6daa46
shaynedsouza <shaynedsouza@hotmail.com>
2024-11-07 09:20:48 +0530

    add tray script

---
+++ Assets/DarkRoomVRRoot/Scripts/Grabbables/Tray.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LiquidVolumeFX;
+
+public class Tray : MonoBehaviour
+{
+    [SerializeField] private LiquidVolume lv;
+    [SerializeField] private float totalFillThreshold;
+    List<Color> liquidColours;
+    void Start()
+    {
+        liquidColours = new List<Color>();
+        if (!lv) lv = GetComponentInChildren<LiquidVolume>();
+        lv.level = 0f;
+        lv.alpha = 0f;
+    }
+
+    public void AddLiquid()
+    {
+        if (lv.level >= totalFillThreshold) return;
+
+        lv.alpha = 1f;
+        lv.level += 0.05f;
+    }
+
+
+}
