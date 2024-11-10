116da82597ae687e015ff1ca80f9ae1106f3039c
shaynedsouza <shaynedsouza@hotmail.com>
2024-11-10 12:18:44 +0530

    reduce drop scale

--- Assets/DarkRoomVRRoot/Scripts/Grabbables/DropperCap.cs
+++ Assets/DarkRoomVRRoot/Scripts/Grabbables/DropperCap.cs
@@ -43,20 +43,7 @@ public class DropperCap : MonoBehaviour
 
         Debug.Log("Spawned drop");
         GameObject oneSpill = Instantiate(spillPrefab, spillPoint.position, Quaternion.identity) as GameObject;
-        // oneSpill.transform.localScale *= Random.Range(0.45f, 0.65f);
         oneSpill.GetComponent<Renderer>().material.color = lv.liquidColor1;
-
-        // Rigidbody rb = oneSpill.GetComponent<Rigidbody>();
-        // rb.transform.position = spillPos + Random.insideUnitSphere * 0.01f;
-        // rb.AddForce(new Vector3(Random.value - 0.5f, Random.value * 0.1f - 0.2f, Random.value - 0.5f));
-        //rb.AddForce(new Vector3(0f, Random.value * 0.1f - 0.2f, 0f));
-        // StartCoroutine(DestroySpill(oneSpill));
-
     }
 
-    // IEnumerator DestroySpill(GameObject spill)
-    // {
-    //     yield return new WaitForSeconds(1f);
-    //     Destroy(spill);
-    // }
 }
