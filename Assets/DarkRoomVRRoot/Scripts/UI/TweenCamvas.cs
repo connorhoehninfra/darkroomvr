4adcc9bb214ada6b4959791b5d6323c833d4d376
shaynedsouza <shaynedsouza@hotmail.com>
2024-11-15 17:52:25 +0530

    add passthrough and canvas tween

---
+++ Assets/DarkRoomVRRoot/Scripts/UI/TweenCamvas.cs
@@ -0,0 +1,54 @@
+
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TweenCamvas : MonoBehaviour
+{
+    Transform myCamera;
+    [SerializeField] private float m_CamvasOffsetDistance = 1f;
+    [SerializeField] private float m_camvasMoveSpeed = 1f;
+    public float dottweenlimit = 1f;
+
+    float m_height;
+    void Start()
+    {
+        myCamera = Camera.main.transform;
+        m_height = myCamera.transform.position.y;
+
+
+        //Set the initial position
+
+        // Position
+        var cameraOffset = myCamera.transform.forward * m_CamvasOffsetDistance;
+        cameraOffset.y = m_height;
+        transform.position = myCamera.transform.position + cameraOffset;
+
+        //Rotation
+        var targetDirection = myCamera.transform.position - transform.position;
+        targetDirection.y = 0f;
+        var targetRotation = Quaternion.LookRotation(-targetDirection, Vector3.up);
+        transform.rotation = targetRotation;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Rotation
+        var targetDirection = myCamera.transform.position - transform.position;
+        targetDirection.y = 0f;
+        var targetRotation = Quaternion.LookRotation(-targetDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime);
+
+
+        if (Vector3.Dot(myCamera.transform.forward, transform.forward) > dottweenlimit) return;
+
+        // Position
+        var cameraOffset = myCamera.transform.forward * m_CamvasOffsetDistance;
+        cameraOffset.y = m_height;
+        transform.position = Vector3.Lerp(transform.position, myCamera.transform.position + cameraOffset, Time.deltaTime * m_camvasMoveSpeed);
+
+    }
+}
